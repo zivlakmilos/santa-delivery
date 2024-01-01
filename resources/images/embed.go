aed346f99e8b785556c2a00bfd086edc523dd089
Milos Zivlak <zivlakmilos@gmail.com>
2024-01-01 23:56:58 +0100

    Add tiles and objects resources

--- resources/images/embed.go
+++ resources/images/embed.go
@@ -6,3 +6,87 @@ import (
 
 //go:embed bg.png
 var BackgroundPng []byte
+
+//go:embed tiles/1.png
+var Tile1Png []byte
+
+//go:embed tiles/2.png
+var Tile2Png []byte
+
+//go:embed tiles/3.png
+var Tile3Png []byte
+
+//go:embed tiles/4.png
+var Tile4Png []byte
+
+//go:embed tiles/5.png
+var Tile5Png []byte
+
+//go:embed tiles/6.png
+var Tile6Png []byte
+
+//go:embed tiles/7.png
+var Tile7Png []byte
+
+//go:embed tiles/8.png
+var Tile8Png []byte
+
+//go:embed tiles/9.png
+var Tile9Png []byte
+
+//go:embed tiles/10.png
+var Tile10Png []byte
+
+//go:embed tiles/11.png
+var Tile11Png []byte
+
+//go:embed tiles/12.png
+var Tile12Png []byte
+
+//go:embed tiles/13.png
+var Tile13Png []byte
+
+//go:embed tiles/14.png
+var Tile14Png []byte
+
+//go:embed tiles/15.png
+var Tile15Png []byte
+
+//go:embed tiles/16.png
+var Tile16Png []byte
+
+//go:embed tiles/17.png
+var Tile17Png []byte
+
+//go:embed tiles/18.png
+var Tile18Png []byte
+
+//go:embed objects/Crate.png
+var CreatePng []byte
+
+//go:embed objects/Crystal.png
+var CrystalPng []byte
+
+//go:embed objects/IceBox.png
+var IceBoxPng []byte
+
+//go:embed objects/Igloo.png
+var IglooPng []byte
+
+//go:embed objects/Sign_1.png
+var Sign1Png []byte
+
+//go:embed objects/Sign_2.png
+var Sign2Png []byte
+
+//go:embed objects/SnowMan.png
+var SnowManPng []byte
+
+//go:embed objects/Stone.png
+var StonePng []byte
+
+//go:embed objects/Tree_1.png
+var Tree1Png []byte
+
+//go:embed objects/Tree_2.png
+var Tree2Png []byte
