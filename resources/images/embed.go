c5c19844e34764edf1d26607ac6ec067b37cd52a
Milos Zivlak <zivlakmilos@gmail.com>
2024-01-10 22:56:08 +0100

    Santa character sprites

--- resources/images/embed.go
+++ resources/images/embed.go
@@ -90,3 +90,207 @@ var Tree1Png []byte
 
 //go:embed objects/Tree_2.png
 var Tree2Png []byte
+
+//go:embed characters/idle_1.png
+var SantaIdle1 []byte
+
+//go:embed characters/idle_2.png
+var SantaIdle2 []byte
+
+//go:embed characters/idle_3.png
+var SantaIdle3 []byte
+
+//go:embed characters/idle_4.png
+var SantaIdle4 []byte
+
+//go:embed characters/idle_5.png
+var SantaIdle5 []byte
+
+//go:embed characters/idle_6.png
+var SantaIdle6 []byte
+
+//go:embed characters/idle_7.png
+var SantaIdle7 []byte
+
+//go:embed characters/idle_8.png
+var SantaIdle8 []byte
+
+//go:embed characters/idle_9.png
+var SantaIdle9 []byte
+
+//go:embed characters/idle_10.png
+var SantaIdle10 []byte
+
+//go:embed characters/idle_11.png
+var SantaIdle11 []byte
+
+//go:embed characters/idle_12.png
+var SantaIdle12 []byte
+
+//go:embed characters/idle_13.png
+var SantaIdle13 []byte
+
+//go:embed characters/idle_14.png
+var SantaIdle14 []byte
+
+//go:embed characters/idle_15.png
+var SantaIdle15 []byte
+
+//go:embed characters/idle_16.png
+var SantaIdle16 []byte
+
+//go:embed characters/run_1.png
+var SantaRun1 []byte
+
+//go:embed characters/run_2.png
+var SantaRun2 []byte
+
+//go:embed characters/run_3.png
+var SantaRun3 []byte
+
+//go:embed characters/run_4.png
+var SantaRun4 []byte
+
+//go:embed characters/run_5.png
+var SantaRun5 []byte
+
+//go:embed characters/run_6.png
+var SantaRun6 []byte
+
+//go:embed characters/run_7.png
+var SantaRun7 []byte
+
+//go:embed characters/run_8.png
+var SantaRun8 []byte
+
+//go:embed characters/run_9.png
+var SantaRun9 []byte
+
+//go:embed characters/run_10.png
+var SantaRun10 []byte
+
+//go:embed characters/run_11.png
+var SantaRun11 []byte
+
+//go:embed characters/walk_1.png
+var SantaWalk1 []byte
+
+//go:embed characters/walk_2.png
+var SantaWalk2 []byte
+
+//go:embed characters/walk_3.png
+var SantaWalk3 []byte
+
+//go:embed characters/walk_4.png
+var SantaWalk4 []byte
+
+//go:embed characters/walk_5.png
+var SantaWalk5 []byte
+
+//go:embed characters/walk_6.png
+var SantaWalk6 []byte
+
+//go:embed characters/walk_7.png
+var SantaWalk7 []byte
+
+//go:embed characters/walk_8.png
+var SantaWalk8 []byte
+
+//go:embed characters/walk_9.png
+var SantaWalk9 []byte
+
+//go:embed characters/walk_10.png
+var SantaWalk10 []byte
+
+//go:embed characters/walk_11.png
+var SantaWalk11 []byte
+
+//go:embed characters/walk_12.png
+var SantaWalk12 []byte
+
+//go:embed characters/walk_13.png
+var SantaWalk13 []byte
+
+//go:embed characters/slide_1.png
+var SantaSlide1 []byte
+
+//go:embed characters/slide_2.png
+var SantaSlide2 []byte
+
+//go:embed characters/slide_3.png
+var SantaSlide3 []byte
+
+//go:embed characters/slide_4.png
+var SantaSlide4 []byte
+
+//go:embed characters/slide_5.png
+var SantaSlide5 []byte
+
+//go:embed characters/slide_6.png
+var SantaSlide6 []byte
+
+//go:embed characters/slide_7.png
+var SantaSlide7 []byte
+
+//go:embed characters/slide_8.png
+var SantaSlide8 []byte
+
+//go:embed characters/slide_9.png
+var SantaSlide9 []byte
+
+//go:embed characters/slide_10.png
+var SantaSlide10 []byte
+
+//go:embed characters/slide_11.png
+var SantaSlide11 []byte
+
+//go:embed characters/dead_1.png
+var SantaDead1 []byte
+
+//go:embed characters/dead_2.png
+var SantaDead2 []byte
+
+//go:embed characters/dead_3.png
+var SantaDead3 []byte
+
+//go:embed characters/dead_4.png
+var SantaDead4 []byte
+
+//go:embed characters/dead_5.png
+var SantaDead5 []byte
+
+//go:embed characters/dead_6.png
+var SantaDead6 []byte
+
+//go:embed characters/dead_7.png
+var SantaDead7 []byte
+
+//go:embed characters/dead_8.png
+var SantaDead8 []byte
+
+//go:embed characters/dead_9.png
+var SantaDead9 []byte
+
+//go:embed characters/dead_10.png
+var SantaDead10 []byte
+
+//go:embed characters/dead_11.png
+var SantaDead11 []byte
+
+//go:embed characters/dead_12.png
+var SantaDead12 []byte
+
+//go:embed characters/dead_13.png
+var SantaDead13 []byte
+
+//go:embed characters/dead_14.png
+var SantaDead14 []byte
+
+//go:embed characters/dead_15.png
+var SantaDead15 []byte
+
+//go:embed characters/dead_16.png
+var SantaDead16 []byte
+
+//go:embed characters/dead_17.png
+var SantaDead17 []byte
